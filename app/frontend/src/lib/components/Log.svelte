7a36ade1ac8546a69c19b82a5f90356d5e6a0e41
AOzmond <alastair.ozmond@gmail.com>
2025-09-18 05:54:48 -0700

    log autoscroll

--- app/frontend/src/lib/components/Log.svelte
+++ app/frontend/src/lib/components/Log.svelte
@@ -1,10 +1,43 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte"
   import { deviceLogs } from "../../lib/state.svelte"
   import LogRow from "../components/LogRow.svelte"
+
+  let container: HTMLDivElement | null = $state(null)
+  let autoScroll = $state(true)
+  let visibleLogs = $derived(($deviceLogs ?? []).filter(Boolean))
+
+  const scrollToBottom = () => {
+    if (!container) {
+      return
+    }
+    container.scrollTop = container.scrollHeight
+  }
+
+  onMount(() => {
+    scrollToBottom()
+  })
+
+  const handleScroll = () => {
+    if (!container) {
+      return
+    }
+    const distanceFromBottom = container.scrollHeight - container.scrollTop - container.clientHeight
+    autoScroll = distanceFromBottom <= 2
+  }
+
+  $effect(() => {
+    visibleLogs
+    autoScroll
+    if (!autoScroll) {
+      return
+    }
+    scrollToBottom()
+  })
 </script>
 
-<div class="log-container">
-  {#each $deviceLogs as log}
+<div class="log-container" bind:this={container} onscroll={handleScroll}>
+  {#each visibleLogs as log}
     {#if log != null}
       <LogRow {log} />
     {/if}
