f18e1af8e22b9c0eddd7f7764a14354fb41710de
AOzmond <alastair.ozmond@gmail.com>
2025-09-18 09:57:30 -0700

    installed fonts through fontsource

--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -1,5 +1,7 @@
 import App from "./+layout.svelte";
 import { mount } from "svelte";
+import "@fontsource-variable/ibm-plex-sans";
+import "@fontsource-variable/jetbrains-mono";
 
 const app = mount(App, {
   target: document.getElementById("app")!,
